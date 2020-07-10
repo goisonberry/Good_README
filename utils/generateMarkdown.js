82c9b7f297a98b56cde329a73480fa820be036bd
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-07-09 18:15:10 -0700

    added sections that need to populate within the readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,35 @@
-// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const avatar = `https://github.com/${data.username}.png?size=60`;
+  const gitHub = `https://img.shields.io/badge/Github-${data.username}-4cbbb9`;
 
-`;
+  return `
+  # ${data.project} 
+  
+  ## Description
+      ${data.description}
+  ## Table of Contents
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Tests](#tests)
+      - [Contributors](#contributors)
+  ## Installation
+  Packages required to run this program are: 
+  ${data.installation}
+  
+  ## Usage
+  Examples of how to use this program: 
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Tests
+  To test, run the following command:
+  ${data.tests}
+  ## Contributors
+  ${data.contributors}
+  ## Contact
+  \n![Badge](${gitHub}) 
+  \n![Profile Image](${avatar})
+  \nView the project in GitHub at: ${data.url}.`;
 }
-
 module.exports = generateMarkdown;
