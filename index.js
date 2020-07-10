78089800995b0ed3c45820f50b9d1809ca0bc2f6
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-07-09 18:16:05 -0700

    copied code from test.js once code ran without issues.

--- index.js
+++ index.js
@@ -1,16 +1,89 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-];
+//Questions//
 
-// function to write README file
-function writeToFile(fileName, data) {
+function askQuestions() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "project",
+    },
+    {
+      type: "input",
+      message: "Provide URL link for your project.",
+      name: "url",
+    },
+    {
+      type: "input",
+      message: "Description of your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message:
+        "What packages need to be installed in order for your project to run?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "For what purpose can someone use your project?",
+      name: "usage",
+    },
+    {
+      type: "checkbox",
+      message: "What kind of license would you like to use?",
+      name: "license",
+      choices: ["MIT", "APACHE", "GPL", "BSD"],
+    },
+    {
+      type: "input",
+      message: "Technologies Used:",
+      name: "technologies",
+    },
+    {
+      type: "input",
+      message: "Aside from yourself, are there any other contributors?",
+      name: "contributors",
+    },
+    {
+      type: "input",
+      message: "To run a test, what command is needed?",
+      name: "tests",
+    },
+  ]);
 }
 
-// function to initialize program
-function init() {
-
+//This function will generage the answers for the README file//
+function writetoFile(fileName, data) {
+  fs.writeFile(fileName, data, "utf8", function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("You just made a README file!");
+  });
 }
 
-// function call to initialize program
+async function init() {
+  try {
+    const answers = await askQuestions();
+    generateMarkdown(answers);
+    writetoFile("README.md", generateMarkdown(answers));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 init();
